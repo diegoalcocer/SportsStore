006a79bc6e39fc1defca304541523585d00dbb4b
Alcocer Palacios, Diego <DIEGOAL@iadb.org>
2019-04-22 18:48:42 -0400

    DA: Create Store app, models and dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app',
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  template: '<div class="bg-success p-2 text-center text-white"> This is Sport Store</div>'
 })
 export class AppComponent {
   title = 'app';
